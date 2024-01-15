aeb1d753ffb98fd875a9312b474e4a771d47444e
LuanFrrr <luanrosas@yahoo.com.br>
2024-01-15 19:26:50 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Home from "./src/screens/Home/Home";
+import Main from "./main";
 
 export default function App() {
-  return <Home />;
+  return <Main />;
 }
